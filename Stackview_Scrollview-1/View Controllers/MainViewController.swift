c4c909edd95a50e7ad380e742f6fc06fab781cda
Louis Tur <louis.tur@gmail.com>
2017-08-09 16:53:20 -0400

    adds missing storyboard link, print statements

--- Stackview_Scrollview-1/View Controllers/MainViewController.swift
+++ Stackview_Scrollview-1/View Controllers/MainViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class MainViewController: UIViewController {
-  
+	@IBOutlet weak var scrollView: UIScrollView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,14 +22,15 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		print("Content Size: \(self.scrollView.contentSize)")
+		print("ScrollView Bounds: \(self.scrollView.bounds)")
+		print("ScrollView Frame: \(self.scrollView.frame)")
+		
+		print("View Frame: \(self.view.frame)")
+		print("View Bounds: \(self.view.bounds)")
+	}
 
 }
